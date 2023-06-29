d3f95b7031492f956575ee5f058091fc4e5442c4
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-06-29 18:03:19 +0200

    #close11 popolate model and migration

--- app/Models/Specialization.php
+++ app/Models/Specialization.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Specialization extends Model
 {
+    protected $fillable = ['name'];
+
     use HasFactory;
 }
--- database/migrations/2023_06_29_143154_create_specializations_table.php
+++ database/migrations/2023_06_29_143154_create_specializations_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('specializations', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100)->unique();
             $table->timestamps();
         });
     }
