c72bea4ece93e04b6cb28d1d9e6ecae5b9ef483c
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-06-29 16:30:14 +0200

    create Vote Model

---
+++ app/Policies/VotePolicy.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Vote;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class VotePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Vote  $vote
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, Vote $vote)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Vote  $vote
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Vote $vote)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Vote  $vote
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Vote $vote)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Vote  $vote
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, Vote $vote)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Vote  $vote
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, Vote $vote)
+    {
+        //
+    }
+}
