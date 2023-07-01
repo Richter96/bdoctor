b98a539eedf77d11832b85a2d8864afa69e1f69d
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-07-01 11:16:41 +0200

    create show doctor #3

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,9 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+
+    // $slug = Auth::user()->doctor->slug;
+    public const HOME = '/dashboard/doctor';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
