3c8766bcb8a6388f8cea3ea46652d1da38215427
cyberfolk <andrea1995ferrari@gmail.com>
2023-07-02 12:15:10 +0200

    Make clickable dashboard proifile message review

--- resources/views/partials/sidebar_menu.blade.php
+++ resources/views/partials/sidebar_menu.blade.php
@@ -1,35 +1,48 @@
+{{-- 
+    Per evidenziare l'elemento dellla side_bar selezionato
+    Abbiamo usato str_starts_with anzichè un semplice "==" in modo che il nav-item rimanesse selezionato anche durante le CRUD quando l'url potrebbe cambiare leggermente
+ --}}
+
 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light collapse position-sticky pt-3">
     <ul class="nav flex-column">
         <li class="nav-item">
-            MY Profile
-            {{--  <a class="nav-link {{ Route::currentRouteName() == 'admin.dashboard' ? 'bg-dark' : '' }}" aria-current="page" href="{{ route('admin.dashboard') }}">
-                <i class="fa-solid fa-gauge me-1"></i>
+            {{-- VEDI IN ALTO --}}
+            <a class="nav-link {{ str_starts_with(Route::currentRouteName(), 'dashboard') ? 'bg-dark' : '' }}" aria-current="page" href="{{ route('dashboard') }}">
+                <i class="me-2 fa-solid fa-gauge-high"></i>
                 {{ __('Dashboard') }}
-            </a> --}}
+            </a>
         </li>
         <!-- nav-item -->
         <li class="nav-item">
-            Reviews
-            {{-- <a class="nav-link {{ Route::currentRouteName() == 'admin.projects.index' ? 'bg-dark' : '' }}" href="{{ route('admin.projects.index') }}">
-                <i class="fa-solid fa-thumbtack me-1"></i>
-                {{ __('Projects') }}
-            </a> --}}
+            {{-- VEDI IN ALTO --}}
+            <a class="nav-link {{ str_starts_with(Route::currentRouteName(), 'doctor') ? 'bg-dark' : '' }}" aria-current="page" href="{{ route('doctor.show', $doctor) }}">
+                <i class="me-2 fa-solid fa-user"></i>
+                {{ __('MY Profile') }}
+            </a>
         </li>
         <!-- nav-item -->
         <li class="nav-item">
-            Messages
-            {{-- <a class="nav-link {{ Route::currentRouteName() == 'admin.types.index' ? 'bg-dark' : '' }}" href="{{ route('admin.types.index') }}">
-                <i class="fa-solid fa-bookmark me-1"></i>
-                {{ __('Type') }}
-            </a> --}}
+            {{-- VEDI IN ALTO --}}
+            <a class="nav-link {{ str_starts_with(Route::currentRouteName(), 'review.') ? 'bg-dark' : '' }}" href="{{ route('review.index') }}">
+                <i class="me-2 fa-solid fa-book-open-reader"></i>
+                {{ __('Reviews') }}
+            </a>
         </li>
         <!-- nav-item -->
         <li class="nav-item">
-            Statistics
-            {{-- <a class="nav-link {{ Route::currentRouteName() == 'admin.technologies.index' ? 'bg-dark' : '' }}" href="{{ route('admin.technologies.index') }}">
-                <i class="fa-solid fa-bookmark me-1"></i>
-                {{ __('Technologies') }}
-            </a> --}}
+            {{-- VEDI IN ALTO --}}
+            <a class="nav-link {{ str_starts_with(Route::currentRouteName(), 'message') ? 'bg-dark' : '' }}" href="{{ route('message.index') }}">
+                <i class="me-2 fa-solid fa-message"></i>
+                {{ __('Messages') }}
+            </a>
+        </li>
+        <!-- nav-item -->
+        <li class="nav-item">
+            {{-- VEDI IN ALTO --}}
+            <a class="nav-link {{ str_starts_with(Route::currentRouteName(), 'statistic') ? 'bg-dark' : '' }}" {{-- href="{{ route('statistic.index') }}" --}}>
+                <i class="me-2 fa-solid fa-chart-simple"></i>
+                {{ __('Statistics') }}
+            </a>
         </li>
         <!-- nav-item -->
     </ul>
