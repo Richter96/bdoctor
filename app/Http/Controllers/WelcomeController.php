08fddda6b8d216dc783046a5396f204fb3c46a03
cyberfolk <andrea1995ferrari@gmail.com>
2023-07-04 17:44:46 +0200

    #50 Show in welcome doctor-card

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Specialization;
 use App\Models\Doctor;
+use App\Models\User;
 
 class WelcomeController extends Controller
 {
@@ -16,9 +17,22 @@ class WelcomeController extends Controller
 
     public function show(Request $request)
     {
-        $specialization_id_selected = $request->request->get('specializations');
+        $request->validate([
+            'specializations' => ['required', 'numeric'],
+        ]);
         
-        dd($specialization_id_selected);
-        return view('welcome');
+        $specialization_id_selected = $request->request->get('specializations');
+        $specialization = Specialization::find($specialization_id_selected);
+        $doctors_id_specializations = $specialization->doctors->pluck('id')->toArray();
+        $doctors = [];
+        $users = [];
+        foreach ($doctors_id_specializations as $id) {
+            array_push($doctors, Doctor::find($id));
+            array_push($users, User::find($id));
+        }
+
+        $specializations = Specialization::all();
+
+        return view('welcome', compact('doctors','users', 'specializations'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,27 +7,62 @@
         </div>
     </div>
 
-    <div class="content">
-        <div class="container text-center">
-            <h3>Che specialista desideri?</h3>
+    <div class="container">
+        <div class="container">
+            <h3 class="mb-3 mt-5">Che specialista desideri?</h3>
 
             <form action="{{ route('form-specialization') }}" method="post">
                 @csrf
-                <div class="mb-3">
-                    <label for="" class="form-label">Specializazzione</label>
-                    <select class="form-select form-select-lg" name="specializations" id="specializations">
-                        <option selected>Select one</option>
+                <div class="input-group mb-3">
+                    <select required class="form-select form-select-lg" name="specializations" id="specializations">
+                        <option disabled selected>Select one</option>
                         @foreach ($specializations as $specialization)
                             <option value="{{ $specialization->id }}">{{ $specialization->name }}</option>
                         @endforeach
                     </select>
+                    <button type="submit" class="input-group-text">Submit</button>
                 </div>
-                <div class="">
-                    <button type="submit" class="btn btn-light text-dark px-4">Search</button>
-                </div>
-            </form>
 
-            <div>{{ $specialization->name }}</div>
+                @error('specializations')
+                    <div class="my-3"><small class="text-danger">{{ $message }}</small></div>
+                @enderror
+            </form>
         </div>
+
+        @if (isset($doctors) && !empty($doctors))
+            <div class="row">
+                @foreach ($doctors as $index => $doctor)
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h2 class="card-title m-0 text-uppercase me-3 p-0 ">{{ $users[$index]->name }} {{ $users[$index]->lastname }}</h2>
+                                <div>
+                                    <p><strong>Phone number: </strong>{{ $doctor->phone }}</p>
+                                </div>
+                                <div>
+                                    <p><strong>Email: </strong>{{ $users[$index]->email }}</p>
+                                </div>
+                                <p><strong>Prestazioni: </strong>{{ $doctor->service }}</p>
+                                <p><strong>Indirizzo </strong>{{ $doctor->address }}</p>
+                                <span><strong>Specializzazioni:</strong></span>
+                                <ul>
+                                    @forelse ($doctor->specializations as $specialization)
+                                        <li>{{ $specialization->name }}</li>
+                                    @empty
+                                        <li class=" list-unstyled">nessuna specializzazione</li>
+                                    @endforelse
+                                </ul>
+                            </div>
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col -->
+                @endforeach
+            </div>
+            <!-- /.row -->
+        @else
+            <div>Nessun risultato</div>
+        @endif
+
     </div>
 @endsection
