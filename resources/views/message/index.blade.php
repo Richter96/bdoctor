1d291f026c9d1f18eddbff06ed5fddc98661f6a2
MartinaDR1 <derosemartina933@gmail.com>
2023-07-06 15:40:17 +0200

    add modal show

--- resources/views/message/index.blade.php
+++ resources/views/message/index.blade.php
@@ -9,7 +9,7 @@
                     <th>Email</th>
                     <th>Messaggio</th>
                     <th>Data</th>
-                    <th>Action</th>
+                    <th>Actions</th>
                 </tr>
             </thead>
             <tbody>
@@ -20,13 +20,33 @@
                     <td>{{$message->text}}</td>
                     <td>{{$message->date_time}}</td>
                     <td>
+                        <button type="submit" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">Show</button>
                         <form action="{{route('message.destroy', $message)}}" method="POST">
                             @csrf
                             @method('DELETE')
                             <button type="submit" class="btn btn-danger">Delete</button>
                         </form>
                     </td>
-
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <div class="text">
+                                        <h1 class="modal-title fs-4" id="exampleModalLabel">{{$message->name_patient}}</h1>
+                                        <h3 class="text-muted fs-6" id="exampleModalLabel">{{$message->email_patient}}</h3>
+                                    </div>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                    {{$message->text}}
+                                </div>
+                                <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
                 </tr>
             </tbody>
             @empty
