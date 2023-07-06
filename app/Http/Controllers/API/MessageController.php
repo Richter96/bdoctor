a6b43bb2f4f35d67c28f147c6f8caed4d76c4f0f
Daniele Morello <85832477+DanieleMorello@users.noreply.github.com>
2023-07-06 18:17:53 +0200

    Add ApiMessageController

--- app/Http/Controllers/API/MessageController.php
+++ app/Http/Controllers/API/MessageController.php
@@ -4,8 +4,34 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Message;
+use Illuminate\Support\Facades\Validator;
 class MessageController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'name_patient' => 'required',
+            'email_patient' => 'email',
+            'text' => 'required',
+            'doctor_id' => 'numeric',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        $new_message = new Message();
+        $new_message->fill($data);
+        $new_message->save();
+
+        return response()->json([
+            'success' => true,
+        ]);
+    }
 }
