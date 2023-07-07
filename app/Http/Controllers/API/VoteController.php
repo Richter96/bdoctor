bd10b61f82fa5136db2e0c512656d8f7a96b648f
cyberfolk <andrea1995ferrari@gmail.com>
2023-07-07 12:55:43 +0200

    Add API post to vote

--- app/Http/Controllers/API/VoteController.php
+++ app/Http/Controllers/API/VoteController.php
@@ -4,8 +4,34 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Doctor;
+use App\Models\Vote;
+use Illuminate\Support\Facades\Validator;
 
 class VoteController extends Controller
 {
-    //
-}
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+       $validator = Validator::make($data, [
+            'vote' => 'required',
+            'doctor_id' => 'numeric',
+        ]);
+
+        $doctor = Doctor::find($request->doctor_id);
+        $vote = Vote::find($request->vote); // Dovrebbe essere vote_id ma fortunatamente vote e vote_id sono in corrispondenza biunivoca
+
+        $doctor->votes()->attach($vote);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        return response()->json([
+            'success' => true,
+        ]);
+    }}
