daf27e4cf174602e51675cbd01d94cc5da5496db
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-07-14 13:05:54 +0200

    integrate add sponsorship with edit profile

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -8,10 +8,10 @@ use App\Models\Doctor;
 use App\Models\Specialization;
 use App\Models\Sponsorship;
 use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
-
-use function PHPUnit\Framework\isEmpty;
 
 class DoctorController extends Controller
 {
@@ -66,6 +66,7 @@ class DoctorController extends Controller
             $cv_doctor = Storage::put('uploads', $val_data['cv']);
             $val_data['cv'] = $cv_doctor;
         }
+
         return view('doctor.show', compact('doctor', 'user'));
     }
 
@@ -129,6 +130,12 @@ class DoctorController extends Controller
         $user->lastname = $val_data['lastname'];
         $user->save();
 
+        //se l'utente ha selezionato una sponsorizzazione
+        if ($request->sponsorship) {
+            $sponsorship_id = $request->sponsorship;
+            $this->add_sponsorship($user_id, $sponsorship_id);
+        }
+
         if ($request->has('specializations')) {
             $doctor->specializations()->sync($request->specializations);
         }
@@ -172,6 +179,62 @@ class DoctorController extends Controller
             ->groupBy('doctors.id')
             ->where('doctors.id', '=', $user_id) // Select the specific user whit user_id
             ->get();
-        return $average; // With [0] i selected the only-one array element 
+
+        return $average; // With [0] i selected the only-one array element
+    }
+
+    private function add_sponsorship($doctor_id, $sponsorship_id)
+    {
+        $sponsorship = Sponsorship::find($sponsorship_id);
+
+        $duration = $sponsorship->duration;
+        $day_duration = $duration / 24;
+
+        $has_sponsorship = DB::table('doctor_sponsorship')->where('doctor_id', '=', $doctor_id)->get();
+
+        // Controlliamo se il dottore ha già fatto delle sponsorizzazioni
+        if (count($has_sponsorship)) {
+            // Recuperiamo la scadenza della sponsorizzazione
+            $sponsorship_id = $has_sponsorship[0]->sponsorship_id;
+            $start_date = $has_sponsorship[0]->start_date;
+            $end_date = $has_sponsorship[0]->end_date;
+            $created_at = $has_sponsorship[0]->created_at;
+            $updated_at = $has_sponsorship[0]->updated_at;
+            $end_date_object = Carbon::createFromFormat('Y-m-d H:i:s', $end_date);
+
+            // Controlliamo se la data di scandenza è maggiore di oggi
+            if ($end_date_object > now()) {
+                $new_end_date = $end_date_object->addDays($day_duration);
+            } else {
+                // Ci troviamo nel caso in cui la sponsorizzazione del dottore sia già scaduta
+                $new_end_date = now()->addDay($day_duration);
+            }
+
+            DB::table('doctor_sponsorship')->upsert(
+                [
+                    'doctor_id' => $doctor_id,
+                    'sponsorship_id' => $sponsorship_id,
+                    'start_date' => $start_date,
+                    'end_date' => $end_date,
+                    'success' => '1',
+                    'created_at' => $created_at,
+                    'updated_at' => $updated_at,
+                ],
+                ['doctor_id', 'sponsorship_id'],
+                ['end_date' => $new_end_date]
+            );
+        } else {
+            // Qui ci ritroviamo nel caso in cui il dottore non abbia mai fatto una sponsorizzazione oppure
+            $new_end_date = now()->addDay($day_duration);
+            DB::table('doctor_sponsorship')->insert([
+                'doctor_id' => $doctor_id,
+                'sponsorship_id' => $sponsorship_id,
+                'start_date' => now(),
+                'end_date' => $new_end_date,
+                'success' => '1',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
